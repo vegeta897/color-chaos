ceae3011b800d8c09bcf82ed653c2f186d0563ac
vegeta897 <vegeta897@gmail.com>
2013-08-05 05:11:29 -0400

    Improved pixel highlight logic, added "latest colors" on bottom

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,12 @@ angular.module('ColorChaos.controllers', [])
 	.controller('ColorGrid', ['$scope', '$timeout', 'utility', function($scope, $timeout, utility) {
         
         $scope.yourChanges = 0; // Tracking how many pixels you've changed
+        $scope.lastColors = []; // Tracking your last colors
         
         // Set up some globals
-        var pixSize = 10, overPixel = [], overColor = '', mouseDown = 0;
+        var pixSize = 10, overPixel = [], mouseDown = 0;
 
-        // Create a reference to the pixel data for our drawing.
+        // Create a reference to the pixel data for our canvas
         var pixelDataRef = new Firebase('https://color-chaos.firebaseio.com');
 
         // Set up our canvas
@@ -19,73 +20,71 @@ angular.module('ColorChaos.controllers', [])
         if(myContext == null) {
             alert('Your browser is really old. Get a new one bro');
         }
-
-        // Keep track of if the mouse is up or down.
-        myCanvas.onmousedown = function() { mouseDown = 1; return false; };
-        myCanvas.onmouseout = myCanvas.onmouseup = function() {
+        var overCanvas = document.getElementById('canvas2'); // Define overCanvas for pixel highlighting
+        var overContext = overCanvas.getContext ? overCanvas.getContext('2d') : null;
+        jQuery(overCanvas).offset(jQuery(myCanvas).offset()); // Set its position to match the real canvas
+        
+        // Keep track of if the mouse is up or down
+        overCanvas.onmousedown = function() { mouseDown = 1; return false; };
+        overCanvas.onmouseout = overCanvas.onmouseup = function() {
             mouseDown = 0; 
         };
 
         // Disable text selection.
         myCanvas.onselectstart = function() { return false; };
 
+        // Add the drawn pixel color to the lastColors array
+        var addLastColor = function(color) {
+            if($scope.lastColors.length >= 24) { // Don't exceed 24 last colors
+                $scope.lastColors.pop();
+            }
+            $scope.lastColors.unshift('#'+color);
+        }
+        
         // Draw a pixel of random color on the mouse's position
-        var drawOnMouseDown = function(e) {
-            if (!mouseDown) return;
-
-            var offset = jQuery('canvas').offset();
-            var x1 = Math.floor((e.pageX - offset.left) / pixSize),
-                y1 = Math.floor((e.pageY - offset.top) / pixSize);
+        var drawOnMouseDown = function() {
+            if (!mouseDown) return; // If the mouse button is down, cancel
             
-            // Write the pixel into Firebase, or if we are drawing white, remove the pixel.
+            // Write the pixel into Firebase
             var randomColor = utility.generate();
-            overColor = randomColor;
-            pixelDataRef.child(x1 + ":" + y1).set(randomColor);
-
+            addLastColor(randomColor); // Add to last colors
+            pixelDataRef.child(overPixel[0] + ":" + overPixel[1]).set(randomColor);
         };
-
         // Check for mouse moving to new pixel
         var onMouseMove = function(e) {
             // Get pixel location
-            var offset = jQuery('canvas').offset();
+            var offset = jQuery(overCanvas).offset();
             var x = Math.floor((e.pageX - offset.left) / pixSize),
                 y = Math.floor((e.pageY - offset.top) / pixSize);
             // If the pixel location has changed
             if(overPixel[0] != x || overPixel[1] != y) {
                 dimPixel(); // Dim the last pixel
                 overPixel = [x,y]; // Update the pixel location we're now over
-                var overRGB = myContext.getImageData(x*pixSize,y*pixSize,1,1).data; // Update the color we're now over
-                overColor = '#' + ("000000" + utility.rgbToHex(overRGB[0],overRGB[1],overRGB[2])).slice(-6);
-                highlightPixel(overRGB); // Highlight this pixel
+                highlightPixel(); // Highlight this pixel
             }
         };
-        
         // Dim the pixel after leaving it
         var dimPixel = function() {
-            myContext.fillStyle = overColor; // Set color from overColor, which hasn't updated yet
-            myContext.fillRect(overPixel[0] * pixSize, overPixel[1] * pixSize, pixSize, pixSize);
-        }
-        
+            overContext.clearRect(overPixel[0] * pixSize, overPixel[1] * pixSize, pixSize, pixSize);
+        };
         // Highlight the pixel underneath the mouse
-        var highlightPixel = function(rgb) {
-            var highlighted = [];
-            for(var i = 0; i<3; i++) { // Brighten the color
-                highlighted[i] = rgb[i] + 20;
-                if(highlighted[i] > 255) {
-                    highlighted[i] = 255;
-                }
-            }
+        var highlightPixel = function() {
             // Draw the highlighted color pixel
-            myContext.fillStyle = '#' + ("000000" + utility.rgbToHex(highlighted[0],highlighted[1],highlighted[2])).slice(-6);
-            myContext.fillRect(overPixel[0] * pixSize, overPixel[1] * pixSize, pixSize, pixSize);
-        }
+            overContext.fillStyle = "rgba(255, 255, 255, 0.15)";
+            overContext.fillRect(overPixel[0] * pixSize, overPixel[1] * pixSize, pixSize, pixSize);
+        };
+        // When the mouse button is pressed (on the overCanvas)
+        var overMouseDown = function() {
+            jQuery(myCanvas).mousedown(); // Echo the event to the real canvas
+        };
         
-        jQuery(myCanvas).mouseout(dimPixel);
+        jQuery(overCanvas).mousemove(onMouseMove);
+        jQuery(overCanvas).mouseout(dimPixel);
+        jQuery(overCanvas).mousedown(overMouseDown); // Will send to real canvas
         jQuery(myCanvas).mousedown(drawOnMouseDown);
-        jQuery(myCanvas).mousemove(onMouseMove);
 
-        // Add callbacks that are fired any time the pixel data changes and adjusts the canvas appropriately.
-        // Note that child_added events will be fired for initial pixel data as well.
+        // Add callbacks that are fired any time the pixel data changes and adjusts the canvas appropriately
+        // Note that child_added events will be fired for initial pixel data as well
         var drawPixel = function(snapshot) {
             var coords = snapshot.name().split(":");
             myContext.fillStyle = "#" + snapshot.val();
@@ -103,80 +102,3 @@ angular.module('ColorChaos.controllers', [])
         pixelDataRef.on('child_removed', clearPixel);
 
     }]);
-
-// firebase drawing demo
-//
-//// Set up some globals
-//var pixSize = 4, lastPoint = null, currentColor = "fc5", mouseDown = 0;
-//
-//// Create a reference to the pixel data for our drawing.
-//var pixelDataRef = getFirebase();
-//
-//// Add clear handler
-//$('#clear').click(function() {
-//    $('#drawing-canvas').get(0).getContext('2d').clearRect(0, 0, 100, 150);
-//    pixelDataRef.set(null);
-//});
-//
-//// Set up our canvas
-//var myCanvas = document.getElementById('drawing-canvas');
-//var myContext = myCanvas.getContext ? myCanvas.getContext('2d') : null;
-//if (myContext == null) {
-//    alert("You must use a browser that supports HTML5 Canvas to run this demo.");
-//}
-//
-//// Keep track of if the mouse is up or down.
-//myCanvas.onmousedown = function () { mouseDown = 1; return false; };
-//myCanvas.onmouseout = myCanvas.onmouseup = function () {
-//    mouseDown = 0, lastPoint = null;
-//};
-//
-//// Disable text selection.
-//myCanvas.onselectstart = function() { return false; };
-//
-//// Draw a line from the mouse's last position to its current position.
-//var drawLineOnMouseMove = function(e) {
-//    if (!mouseDown) return;
-//
-//    // Bresenham's line algorithm. We use this to ensure smooth lines are drawn.
-//    var offset = $('canvas').offset();
-//    var x1 = Math.floor((e.pageX - offset.left) / pixSize - 1),
-//        y1 = Math.floor((e.pageY - offset.top) / pixSize - 1);
-//    var x0 = (lastPoint == null) ? x1 : lastPoint[0];
-//    var y0 = (lastPoint == null) ? y1 : lastPoint[1];
-//    var dx = Math.abs(x1 - x0), dy = Math.abs(y1 - y0);
-//    var sx = (x0 < x1) ? 1 : -1, sy = (y0 < y1) ? 1 : -1, err = dx - dy;
-//    while (true) {
-//        // Write the pixel into Firebase, or if we are drawing white, remove the pixel.
-//        pixelDataRef.child(x0 + ":" + y0).set(currentColor === "fff" ? null : currentColor);
-//
-//        if (x0 == x1 && y0 == y1) break;
-//        var e2 = 2 * err;
-//        if (e2 > -dy) {
-//            err = err - dy;
-//            x0 = x0 + sx;
-//        }
-//        if (e2 < dx) {
-//            err = err + dx;
-//            y0 = y0 + sy;
-//        }
-//    }
-//    lastPoint = [x1, y1];
-//}
-//$(myCanvas).mousemove(drawLineOnMouseMove);
-//$(myCanvas).mousedown(drawLineOnMouseMove);
-//
-//// Add callbacks that are fired any time the pixel data changes and adjusts the canvas appropriately.
-//// Note that child_added events will be fired for initial pixel data as well.
-//var drawPixel = function(snapshot) {
-//    var coords = snapshot.name().split(":");
-//    myContext.fillStyle = "#" + snapshot.val();
-//    myContext.fillRect(parseInt(coords[0]) * pixSize, parseInt(coords[1]) * pixSize, pixSize, pixSize);
-//}
-//var clearPixel = function(snapshot) {
-//    var coords = snapshot.name().split(":");
-//    myContext.clearRect(parseInt(coords[0]) * pixSize, parseInt(coords[1]) * pixSize, pixSize, pixSize);
-//}
-//pixelDataRef.on('child_added', drawPixel);
-//pixelDataRef.on('child_changed', drawPixel);
-//pixelDataRef.on('child_removed', clearPixel);
