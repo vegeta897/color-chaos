3f52ca1ac26bb542ee4ba90f4f57ab5034606565
vegeta897 <vegeta897@gmail.com>
2013-08-05 01:40:46 -0400

    Client-state removed

--- js/controllers.js
+++ js/controllers.js
@@ -16,15 +16,12 @@ angular.module('ColorChaos.controllers', [])
                     $scope.fireColors.colors1 = randomColor.initFirebase(80,96); // rows,cols - use 96 cols @ 10px
                 }
             }
-            $scope.viewedColors = jQuery.extend(true, {}, $scope.fireColors);
             $scope.$watch('fireColors', function() {
                 console.log('watch event!');
-                $scope.viewedColors = jQuery.extend(true, {}, $scope.fireColors);
             })
             $scope.changeColor = function(row,index) {
                 var edited = randomColor.generate();
                 console.log('changing color',row,index,'to',edited);
-                $scope.viewedColors.colors1[row].colors[index] = edited;
                 $scope.fireColors.colors1[row].colors[index] = edited;
                 $scope.yourChanges++;
             }
